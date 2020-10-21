4a642cf7bf6d74abe28460c26966e7bb9eecb3bc
Jonny Kates <jonnykates@gmail.com>
2020-10-21 14:26:06 +0100

    Format some dates

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -4,4 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  eleventyConfig.addFilter("monthDateFormat", (dateObj) => {
+    return new Date(dateObj).toLocaleString("en-gb", {
+      year: "numeric",
+      month: "long",
+    });
+  });
 };
